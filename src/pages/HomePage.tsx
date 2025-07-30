b5dfc99ef42bf895b9bdaacdf9b3657c37ca7ab2
Felipe A Matos <felipearmat@gmail.com>
2025-07-30 18:22:03 -0300

    Feat: Adds patient search to home page
    Adds patient search functionality to the home page.
    
    Fetches and displays a list of practitioners, and allows users to search for patients based on name, birthdate, gender, and general practitioner.

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,34 +1,40 @@
+import { JSX, useEffect, useState } from 'react';
 import { Title } from '@mantine/core';
-import { getReferenceString } from '@medplum/core';
+import { SearchRequest } from '@medplum/core';
 import { Practitioner } from '@medplum/fhirtypes';
-import { Document, ResourceName, SearchControl, useMedplumNavigate, useMedplumProfile } from '@medplum/react';
-import { JSX } from 'react';
-import { Outlet } from 'react-router';
+import {
+  Document,
+  ResourceName,
+  useMedplumProfile,
+  useMedplum,
+  SearchControl,
+} from '@medplum/react';
 
-/**
- * Home page that greets the user and displays a list of patients.
- * @returns A React component that displays the home page.
- */
 export function HomePage(): JSX.Element {
-  // useMedplumProfile() returns the "profile resource" associated with the user.
-  // This can be a Practitioner, Patient, or RelatedPerson depending on the user's role in the project.
-  // See the "Register" tutorial for more detail
-  // https://www.medplum.com/docs/tutorials/register
   const profile = useMedplumProfile() as Practitioner;
+  const medplum = useMedplum();
+  const [search, setSearch] = useState<SearchRequest>({
+    resourceType: 'Patient',
+    fields: ['name', 'birthDate', 'gender', 'generalPractitioner']
+  });
+  const [practitioners, setPractitioners] = useState<Practitioner[]>([]);
 
-  const navigate = useMedplumNavigate();
+  useEffect(() => {
+    async function fetchPractitioners() {
+      const results = await medplum.searchResources('Practitioner');
+      setPractitioners(results);
+    }
+
+    fetchPractitioners();
+  }, [medplum]);
 
   return (
     <Document>
-      <Title>
+      <Title mb="md">
         Welcome <ResourceName value={profile} link />
       </Title>
-      <SearchControl
-        search={{ resourceType: 'Patient', fields: ['name', 'birthdate', 'gender'] }}
-        onClick={(e) => navigate(`/${getReferenceString(e.resource)}`)}
-        hideToolbar
-      />
-      <Outlet />
+
+      <SearchControl search={search} hideToolbar={true} onChange={e => setSearch(e.definition)} />
     </Document>
   );
 }
